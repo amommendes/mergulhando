cdf6f3170520c2ad6e5792a8b788f9f0b8fd2b27
amommendes <amommendes@hotmail.com>
2018-07-09 23:44:41 -0300

    Models

--- mergulhando/alunos/models.py
+++ mergulhando/alunos/models.py
@@ -7,7 +7,7 @@ class Alunos(models.Model):
 	id_aluno    = models.AutoField(primary_key=True, unique=True)
 	nome        = models.CharField(max_length=100)
 	telefone    = models.CharField(max_length=15)
-	celula      = models.ForeignKey('Celula')
+	celula      = models.ForeignKey('Celula', on_delete=models.SET_NULL)
 	email       = models.EmailField()
 	observacoes = models.CharField(max_length=300)
 	def __str__ (self):
@@ -40,21 +40,21 @@ class Cursos(models.Model):
 
 class Modulo(models.Model):
 	id_modulo = models.AutoField(primary_key=True, unique=True)
-	curso     = models.ForeignKey('Cursos')
+	curso     = models.ForeignKey('Cursos', on_delete=models.SET_NULL)
 	nome      = models.CharField(max_length=100)
 	ativo     = models.BooleanField(default=True)
 
 class Turma(models.Model):
 	id_turma  = models.AutoField(primary_key=True, unique=True)
-	curso     = models.ForeignKey('Cursos')
+	curso     = models.ForeignKey('Cursos',on_delete=models.SET_NULL)
 	nome      = models.CharField(max_length=100)
 	data_ini  = models.DateField()
 	data_fim  = models.DateField()
 
 class Presenca(models.Model):
 	id_modulo = models.AutoField(primary_key=True, unique=True)
-	aluno = models.ForeignKey('Alunos')
-	modulo = models.ForeignKey('Modulo')
+	aluno = models.ForeignKey('Alunos',on_delete=models.CASCADE)
+	modulo = models.ForeignKey('Modulo',on_delete=models.SET_NULL)
 	data  = models.DateField()
 	def __str__(self):
 		return '%s, %s, %s' % (self.curso,self.aluno, self.data)		
